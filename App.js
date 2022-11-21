7f5e1de0c5ec99cd6c9a4e1c7707f0d9eeeeb983
jwills43 <jwills9191@gmail.com>
2022-11-21 17:53:37 +0000

    nav bar

---
+++ App.js
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
