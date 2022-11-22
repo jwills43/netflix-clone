a37ddf19f89cb5aeeaf2148b9a6f4b890a484cde
jwills43 <jwills9191@gmail.com>
2022-11-22 14:47:02 +0000

    nav bar done

--- Requests.js
+++ Requests.js
@@ -1,10 +1,11 @@
 const key = 'd152cb72324186361b4791c001a56785'
 
 const requests = {
-    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}<<api_key>>&language=en-US&page=1`,
-    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}<<api_key>>&language=en-US&page=1`,
-    requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}<<api_key>>`,
-    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}<<api_key>>&language=en-US&page=1`
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestGenre: `https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=en-US`
 }
 
 
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Main from './Main'
+import Row from '../components/Row'
+import requests from '../Requests'
 
-function Home() {
+const Home = () => {
   return (
-    <div>
+    <>
         <Main/>
-    </div>
+        <Row title='Up Coming' fetchURL={requests.requestUpcoming} />
+        <Row title='Popular' fetchURL={requests.requestPopular} />
+        <Row title='Trending' fetchURL={requests.requestTrending} />
+        <Row title='Top Rated' fetchURL={requests.requestTopRated} />
+        <Row title='Genre' fetchURL={requests.requestGenre} />
+    </>
   )
 }
 
