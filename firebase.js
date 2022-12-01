2da5dde87c1af14fda5c73d567bc5b16c3bc6c05
jwills43 <jwills9191@gmail.com>
2022-12-01 17:47:37 +0000

    login page firebase

--- firebase.js
+++ firebase.js
@@ -9,12 +9,14 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
-  projectId:  process.env.REACT_APP_FIREBASE_PROJECT_ID ,
-  storageBucket:  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
-  appId:  process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID
+  
 };
+console.log (process.env)
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
