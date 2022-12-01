f4ba481e5f75866dc976c4b6c6bce9c6bc22e552
jwills43 <jwills9191@gmail.com>
2022-12-01 16:56:08 +0000

    firebase

--- App.js
+++ App.js
@@ -2,15 +2,18 @@ import Navbar from './components/Navbar';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 function App() {
   return (
     <div>
+      <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
--- firebase.js
+++ firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: ProcessingInstruction.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "netflix-react-a29ff.firebaseapp.com",
-  projectId: "netflix-react-a29ff",
-  storageBucket: "netflix-react-a29ff.appspot.com",
-  messagingSenderId: "406455842596",
-  appId: "1:406455842596:web:fd0c15f5deeb3f0302e5b1",
-  measurementId: "G-D0PSPQMVKQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+  projectId:  process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+  storageBucket:  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId:  process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
