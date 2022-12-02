8a613143cf943211f5a4adeb9fd90bfaf03f5326
jwills43 <jwills9191@gmail.com>
2022-12-02 16:44:07 +0000

    login and signup

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import { AuthContextProvider } from './context/AuthContext';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
 
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/account' element={<Account/>} />
       </Routes>
       </AuthContextProvider>
     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth;
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+
+      {user?.email ? (
         <div>
-            <button className='pr-4 text-white'>Sign In</button>
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
-
+          <Link to="/account">
+            <button className="pr-4 text-white">Account</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Log Out
+            </button>
+          </Link>
         </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="pr-4 text-white">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
