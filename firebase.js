05d55db8a3c666e5a82abb5e9efee137ef81acc3
jwills43 <jwills9191@gmail.com>
2022-12-06 15:44:04 +0000

    netflix clone

--- firebase.js
+++ firebase.js
@@ -4,6 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,5 +25,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-
+export const db = getFirestore(app)
